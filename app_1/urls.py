874e05db9c7309782cb56a8b6afe89cb23e5c1ee
hackerspider09 <hackerspider09@gmail.com>
2023-02-20 13:11:28 +0530

    authentication added and pages linked

--- app_1/urls.py
+++ app_1/urls.py
@@ -19,6 +19,11 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('index/', views.index, name='index'),
     path('questions/', views.questions, name='questions'),
     path('result/', views.result, name='result'),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('signout/', views.signout, name='signout'),
+    path('settingwale/', views.settingwale, name='settingwale'),
 ]
--- app_1/views.py
+++ app_1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .models import *
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login , logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url="signin")
 def home(request):
     # s= ExtraField.objects.all()
     # # s=list(s)
@@ -9,9 +13,13 @@ def home(request):
     # player = ExtraField.objects.get(username="prasad")
     # print(player)
     # print(player.p_current_question)
-    return HttpResponse("Hello world")
+    context={
+        "title":"Home",
+        "user":request.user
+    }
+    return render(request,"app_1\home.html",context)
 
-
+@login_required(login_url="signin")
 def questions(request):
     context={
         "title":"Questions",
@@ -32,13 +40,15 @@ def questions(request):
         # player.save()
         return redirect(result)
 
-    print(len(question),"and",player.p_current_question)
+    # print(len(question),"and",player.p_current_question)
     if player.p_current_question >=len(question):
         context["flag"]=False
     # print(request.user.p_current_question)   #working
     context["question"]=Question.objects.get(q_id=player.p_current_question)
     return render(request,"app_1\questions.html",context)
 
+
+@login_required(login_url="signin")
 def result(request):
     context={
         "title":"Result",
@@ -46,3 +56,93 @@ def result(request):
     player = ExtraField.objects.get(username=request.user)
     context["player"]=player
     return render(request,"app_1\\result.html",context)
+
+
+
+def signin(request):
+
+    if request.method == "POST":   #For signin page only username and pass1 taken
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password= pass1)         #authenticate user here
+
+        if user is not None:  #IF correct credentials given
+            login(request, user)
+            if request.user.is_superuser:
+                return redirect("settingwale")
+            fname = user.first_name
+            return render(request, "app_1/home.html",{'fname':fname} )
+
+        else:  #If wrong credentials given
+            messages.error(request, "Bad Credentials")
+            return redirect('signin')
+
+    return render(request,"app_1/login.html")
+
+def index(request):
+    context={
+        "title":"Home page"
+    }
+    return render(request,"app_1\mainhome.html",context)
+
+# @login_required(login_url="signin")
+def signout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request, "Logged out successfully!")
+        return redirect("index")
+    else:
+        return redirect('signin')
+
+def signup(request):
+
+    if request.method == "POST": #Create user details to take them input
+        # username = request.POST.get('username')
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']  #For storing pass2
+
+        if ExtraField.objects.filter(username=username):
+            messages.error(request,"Username already exists ! PLease try Different username")
+            return redirect('home')
+
+        # if ExtraField.objects.filter(email=email):
+        #     messages.error(request,"Email Already Registered!")
+        #     return redirect('home')
+
+        if len(username)>15:
+            messages.error(request,"Username must be under 15 characters")
+
+        if pass1 != pass2:
+            messages.error(request,"Passwords did not match")
+
+        if not username.isalnum():
+            messages.error(request,"Username must be Alpha-Numeric")
+            return redirect('home')
+
+        myuser = ExtraField.objects.create_user(username, email, pass1) #register user in backend
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save() #save user in database
+
+        messages.success(request, "Your account has been successfully created. We have sent you a confirmation email, please confirm your email in order to activate your account.")
+        return redirect('signin')
+
+
+    return render(request,"app_1/signup.html")
+
+
+
+
+@login_required(login_url='login')
+def settingwale(request):
+    context={}
+    players = ExtraField.objects.all()
+    # users = User.objects.all()
+    context["players"]=players
+    # context["users"]=users
+    return render(request,"app_1/settingwale.html",context)
