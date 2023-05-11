675a64e4e2c17e9403bdc90584a42c95bf8bebf5
Devraj Shetake <devrajshetake@gmail.com>
2023-05-12 02:22:33 +0530

    Allowed host changed

--- clash_rc_1/settings.py
+++ clash_rc_1/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 # DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clash.credenz.in', 'www.clash.credenz.in', 'loaclhost', '127.0.0.1']
 # ALLOWED_HOSTS = ['192.168.209.139' , '127.0.0.1' , 'localhost',"*"]
 
 # ALLOWED_HOSTS = ['*']
