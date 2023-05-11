cbd70a24bea8dcce6020e9c16c5dd1cd47602ef5
prasad <hackerspider09@gmail.com>
2023-05-12 02:29:15 +0530

    Bugs fixed But new 1 bug found

--- app_1/urls.py
+++ app_1/urls.py
@@ -19,7 +19,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('index/', views.index, name='index'),
     path('questions/', views.questions, name='questions'),
     path('lifelineActivation/', views.lifelineActivation, name='lifelineActivation'),
     path('quiz-submit/', views.submit, name='submit'),
@@ -28,9 +27,7 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('signout/', views.signout, name='signout'),
-    path('settingwale/', views.settingwale, name='settingwale'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
-    path('test1/', views.test1, name='test1'),
     path('getJSLeaderboard/', views.getJSLeaderboard, name='getJSLeaderboard'),
     path('windowBlurError/', views.windowBlurError, name='windowBlurError'),
     path('webadmin/', views.webadmin, name='webadmin'),
